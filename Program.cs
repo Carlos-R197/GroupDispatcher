b18fbdb77cfd9490f4e2e4bf4042512e6f982dac
Carlos Roque <carlosroque198@gmail.com>
2020-11-08 08:16:03 -0400

    Code and readme added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GroupDispatcher
 {
@@ -6,7 +10,114 @@ namespace GroupDispatcher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            start:
+            Console.Write("Inserte la ruta absoluta del archivo de estudiantes: ");
+            string studentFilePath =  Console.ReadLine();
+            Console.Write("Inserte la ruta absoluta del archivo de temas: ");
+            string subjectFilePath = Console.ReadLine();
+            Console.Write("Inserte la cantidad de grupos: ");
+            int groupsAmount = int.Parse(Console.ReadLine());
+
+            string[] students = File.ReadAllLines(studentFilePath);
+            string[] subjects = File.ReadAllLines(subjectFilePath);
+
+            if (students.Length < groupsAmount || subjects.Length < groupsAmount)
+            {
+                Console.Write("No hay suficientes estudiantes o temas para cada grupo.");
+                Thread.Sleep(1500);
+                Console.Clear();
+                goto start;
+            }
+
+            //Separate the students from their ids, since we only need their names
+            string[] studentsNames = new string[students.Length];
+            char[] charactersToTrim = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-'};
+
+            for (int i = 0; i < studentsNames.Length; i++)
+            {
+                studentsNames[i] = students[i].TrimStart(charactersToTrim);
+            }
+
+            var groups = new Group[groupsAmount];
+            int amountStudentsPerGroup = students.Length / groupsAmount;
+            int amountSubjectPerGroup = subjects.Length / groupsAmount;
+            int extraStudentsAmount = students.Length % groupsAmount;
+            int extraSubjectsAmount = subjects.Length % groupsAmount;
+
+            //Shuffle the students and allocate them into groups
+            var rnd = new Random();
+            var randomStudents = studentsNames.OrderBy(t => rnd.Next()).ToArray();
+            var randomSubjects = subjects.OrderBy(t => rnd.Next()).ToArray();
+
+            var studentsQueue = new Queue<string>(randomStudents);
+            var subjectsQueue = new Queue<string>(randomSubjects);
+
+            for (int i = 0; i < groups.Length; i++)
+            {
+                groups[i] = new Group();
+
+                for (int j = 0; j < amountStudentsPerGroup; j++)
+                    groups[i].students.Add(studentsQueue.Dequeue());
+
+                for (int j = 0; j < amountSubjectPerGroup; j++)
+                    groups[i].subjects.Add(subjectsQueue.Dequeue());
+            }
+
+            //Allocate extra students
+            var groupsIndexesToExclude = new HashSet<int>();
+            for (int i = 0; i < extraStudentsAmount; i++)
+            {
+                int randomGroupIndex = GetRandomNumberExcept(0, groupsAmount - 1, groupsIndexesToExclude);
+                groups[randomGroupIndex].students.Add(studentsQueue.Dequeue());
+                groupsIndexesToExclude.Add(randomGroupIndex);
+            }
+
+            //Allocate extra subjects
+            groupsIndexesToExclude.Clear();
+            for (int i = 0; i < extraSubjectsAmount; i++)
+            {
+                int randomGroupIndex = GetRandomNumberExcept(0, groupsAmount - 1, groupsIndexesToExclude);
+                groups[randomGroupIndex].subjects.Add(subjectsQueue.Dequeue());
+                groupsIndexesToExclude.Add(randomGroupIndex);
+            }
+
+            PrintGroupsData(groups);
+        }
+
+        static int GetRandomNumberExcept(int start, int end, HashSet<int> numbersToExclude)
+        {
+            IEnumerable<int> range = Enumerable.Range(start, end).Where(t => !numbersToExclude.Contains(t));
+            var rnd = new Random();
+            int index = rnd.Next(start, end - numbersToExclude.Count);
+            return range.ElementAt(index);
+        }
+
+        static void PrintGroupsData(Group[] groups)
+        {
+            for (int i = 0; i < groups.Length; i++)
+            {
+                Console.WriteLine($"Grupo {i + 1} (Estudiantes: {groups[i].students.Count}, Temas: {groups[i].subjects.Count})");
+
+                Console.WriteLine("     Estudiantes:");
+                foreach (string student in groups[i].students)
+                    Console.WriteLine($"         {student}");
+                
+                Console.WriteLine("     Temas:");
+                foreach (string subject in groups[i].subjects)
+                    Console.WriteLine($"         {subject}");
+            }
+        }
+    }
+
+    public class Group
+    {
+        public List<string> students { get; set; }
+        public List<string> subjects {get; set; }
+
+        public Group()
+        {
+            students = new List<string>();
+            subjects = new List<string>();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # Group Dispatcher
+
+Group Dispatcher is an app that allow you to get the names of students from a text file, subject from another text file and then randomly organize that information in multiple groups.
+
+Requirements to run the program:
+- Must have .NET Core SDK 3.1. Can be found in the following link: https://dotnet.microsoft.com/download
+
+Once you adquired the SDK download the files in the repository and navigate to the folder in which the files are through the command prompt. After you make it to the folder, use the command "dotnet publish" to obtain the exe and "dotnet run" to run the app.
+
+Example: Let's say you downloaded the files in the following folder: "D:\Downloads\Repos\Group Distpacher".
+Open the command line, navigate to the D: drive and write the following command: "cd D:\Downloads\Repos\Group Distpacher".
+Once there write "dotnet publish" followed with "dotnet run" and the application will start running.
+
+The app initially will ask you for three inputs. The first one is the absolute path of the students file, the second one the absolute path of the subjects file and the third one the amount of groups.
+
+Example:
+- Inserte la ruta completa del archivo de estudiantes: C:\Users\jorge_1anbma0\Desktop\Estudiantes.txt
+- Inserte la ruta completa del archivo de temas: C:\Users\jorge_1anbma0\Desktop\Temas.txt
+- Inserte la cantidad de grupos 6
+
+The app will give you a random result. In my case i got:
+
+    Grupo 1 (Estudiantes: 3, Temas: 1)
+        Estudiantes:
+            Joel
+            Perla
+            Isaac Perez
+        Temas:
+            Uso adecuado de linq en C#
+    Grupo 2 (Estudiantes: 4, Temas: 1)
+        Estudiantes:
+            Tony Alonzo
+            Janet Cristina
+            Jorge
+            Diego Velásquez
+        Temas:
+            Codigo nativo vs codigo en xamarin para desarrollo móvil
+    Grupo 3 (Estudiantes: 3, Temas: 1)
+        Estudiantes:
+            Carlos Jose
+            Jose Del Campo
+            Juansito
+        Temas:
+            Los diferentes comandos de git
+    Grupo 4 (Estudiantes: 3, Temas: 1)
+        Estudiantes:
+            Mariano Vásquez
+            Carlos Rafael
+            Maria
+        Temas:
+            Mejor lenguaje de programacion para crear videojuegos
+    Grupo 5 (Estudiantes: 4, Temas: 1)
+        Estudiantes:
+            Amilkar Fleming
+            Jan Rafael
+            Pedro
+            Carolina
+        Temas:
+            La importa de comer saludable
+    Grupo 6 (Estudiantes: 3, Temas: 1)
+        Estudiantes:
+            Shailyn Alcantara
+            Abril
+            Juan Perez
+        Temas:
+            La política en Republica Dominicana
